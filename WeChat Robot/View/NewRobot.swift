c056b5dfa9fc16c57b1f9a3358f1eef3e1c7fa9e
YangKui <c6swift@gmail.com>
2019-07-09 00:12:49 +0800

    调整 new view 的细节

--- WeChat Robot/View/NewRobot.swift
+++ WeChat Robot/View/NewRobot.swift
@@ -11,18 +11,33 @@ import SwiftUI
 struct NewRobot : View {
   @State var title: String = ""
   @State var url: String = ""
+  @State var hasError = false
+  @State var canSave = false
   
     var body: some View {
       NavigationView {
-        List {
+        Form {
           Section {
             TextField("描述", text: $title)
           }
           Section {
-            TextField("API URL", text: $url)
+            VStack(alignment: .leading) {
+            TextField("API URL", text: $url, onCommit: {
+              if let url = URL(string: self.url),  !self.title.isEmpty {
+                self.canSave = true
+                self.hasError = false
+              } else {
+                print("fuck you")
+                self.hasError = true
+                self.canSave = false
+              }
+            }).textContentType(.URL)
+            hasError ? Text("你必须输入合法的 URL").color(.red) : nil
           }
-        }.navigationBarTitle("New Robot")
-          .listStyle(.grouped)
+          }
+        }
+        .navigationBarTitle("New Robot")
+          .navigationBarItems(trailing: Button(action: {}) { Text("Save") }.disabled(!canSave))
       }
     }
 }
