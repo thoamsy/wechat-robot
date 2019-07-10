74986f177ab618d3655b3883797e4ca7ca617ff2
YangKui <c6swift@gmail.com>
2019-07-10 21:21:13 +0800

    feat: 简单检测下输入的 URL 格式是否正确

--- WeChat Robot/View/NewRobot.swift
+++ WeChat Robot/View/NewRobot.swift
@@ -10,42 +10,58 @@ import SwiftUI
 
 struct NewRobot : View {
   @State var title: String = ""
-  @State var url: String = ""
+  @State var urlString: String = ""
   @State var hasError = false
   @State var canSave = false
   
-    var body: some View {
-      NavigationView {
-        Form {
-          Section {
-            TextField("描述", text: $title)
-          }
-          Section {
-            VStack(alignment: .leading) {
-            TextField("API URL", text: $url, onCommit: {
-              if let url = URL(string: self.url),  !self.title.isEmpty {
-                self.canSave = true
+  private func isValidAPIKey(with url: String) -> Bool {
+    guard
+      let url = URL(string: self.urlString),
+      !self.title.isEmpty,
+      let item = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems?[0],
+      item.name == "key",
+      let _ = item.value
+      else {
+        return false
+    }
+    return true
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("描述", text: $title)
+        }
+        Section {
+          VStack(alignment: .leading) {
+            TextField("API URL", text: $urlString, onCommit: {
+              
+              if (self.isValidAPIKey(with: self.urlString)) {
                 self.hasError = false
+                self.canSave = true
               } else {
-                print("fuck you")
                 self.hasError = true
                 self.canSave = false
               }
             }).textContentType(.URL)
-            hasError ? Text("你必须输入合法的 URL").color(.red) : nil
-          }
+            
+            hasError ? Text("你必须输入符合企业微信机器人协议的 URL").font(.footnote).color(.red) : nil
           }
         }
-        .navigationBarTitle("New Robot")
-          .navigationBarItems(trailing: Button(action: {}) { Text("Save") }.disabled(!canSave))
       }
+      .navigationBarTitle("New Robot")
+      .navigationBarItems(
+        trailing: Button(action: {}) { Text("Save")
+      }.disabled(!canSave))
     }
+  }
 }
 
 #if DEBUG
 struct NewRobot_Previews : PreviewProvider {
-    static var previews: some View {
-        NewRobot()
-    }
+  static var previews: some View {
+    NewRobot()
+  }
 }
 #endif
