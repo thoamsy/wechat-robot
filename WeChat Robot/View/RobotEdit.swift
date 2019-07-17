32fb30d17932617cd1cebb0d9f641a8ad08839aa
YangKui <c6swift@gmail.com>
2019-07-18 00:08:43 +0800

    feat: 添加 navigation view

--- WeChat Robot/View/RobotEdit.swift
+++ WeChat Robot/View/RobotEdit.swift
@@ -21,58 +21,78 @@ struct RobotEdit : View {
     Robot.msgTypes[0]
 
   
-  var body: some View {
-    VStack(alignment: .leading) {
-      SegmentedControl(selection: $selected) {
-        ForEach(Robot.msgTypes.identified(by: \.self)) { type in
-          Text(type.capitalized)
-        }
-      }
-      .frame(height: 100)
-      
-      if selected == Robot.msgTypes[0] {
-        Form {
-          Section(header: Text("内容")) {
-            HStack {
-            MultilineTextView(
-              self.$textContent,
-              placeholder: "通知点啥呢🤔?"
-            )
-              .frame(height: 150)
-              .font(.body)
-              
-            }
-          }
+  private var textView: some View {
+    Form {
+      Section(header: Text("内容")) {
+        HStack {
+          MultilineTextView(
+            self.$textContent,
+            placeholder: "通知点啥呢🤔?"
+          )
+            .frame(height: 150)
+            .font(.body)
           
-          Section(header: Text("配置")) {
-          Toggle("是否 @ 所有人", isOn: $isAll)
-          Toggle("Markdown", isOn: $isMarkdown)
-          }
-        }
-      } else {
-        Form {
-          HStack {
-          Text("标题").bold().frame(width: 100)
-            Divider()
-            TextField("", text: self.$newsTitle)
-          }
-          HStack {
-          Text("描述").bold().frame(width: 100)
-            Divider()
-            TextField("", text: self.$newsDescription)
-          }
-          HStack {
-          Text("图片链接").bold().frame(width: 100)
-            Divider()
-            TextField("", text: self.$newsFigure)
-          }
-          HStack {
-          Text("跳转链接").bold().frame(width: 100)
-            Divider()
-            TextField("", text: self.$newsLink)
-          }
         }
       }
+      
+      Section(header: Text("配置")) {
+        Toggle("是否 @ 所有人", isOn: $isAll)
+        Toggle("Markdown", isOn: $isMarkdown)
+      }
+    }
+  }
+  
+  private var newsView: some View {
+    Form {
+      HStack {
+        Text("标题").bold().frame(width: 100)
+        Divider()
+        TextField("", text: self.$newsTitle)
+      }
+      HStack {
+        Text("描述").bold().frame(width: 100)
+        Divider()
+        TextField("", text: self.$newsDescription)
+      }
+      HStack {
+        Text("图片链接").bold().frame(width: 100)
+        Divider()
+        TextField("", text: self.$newsFigure)
+      }
+      HStack {
+        Text("跳转链接").bold().frame(width: 100)
+        Divider()
+        TextField("", text: self.$newsLink)
+      }
+    }
+  }
+  
+  var body: some View {
+    NavigationView {
+      VStack(alignment: .leading) {
+        SegmentedControl(selection: $selected) {
+          ForEach(Robot.msgTypes.identified(by: \.self)) { type in
+            Text(type.capitalized)
+          }
+        }
+        .frame(height: 100)
+
+        if selected == Robot.msgTypes[0] {
+          textView
+        } else {
+          newsView
+        }
+      }
+      .navigationBarTitle(
+        "编辑",
+        displayMode: .inline
+      )
+      .navigationBarItems(
+        trailing:
+          Button(action: {}) {
+            Text("Done 🚀").color(.black)
+        }
+       )
     }
   }
   
